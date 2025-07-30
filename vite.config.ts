7106ce62feae1ac44d0b36585d2df4b2db77df95
abdellahing <abdallah.maamra@etu.univ-batna2.dz>
2025-07-30 20:00:35 +0100

    all fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8080',
+      '/api': 'https://restaurant-management-system.up.railway.app/'
     },
   },
   build: {
